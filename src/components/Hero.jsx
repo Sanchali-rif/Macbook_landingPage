a6c528ee09fa221c0fe5da427be22c15c1e32cb9
Sanchali-rif <sanchalisaha05@gmail.com>
2026-05-04 20:35:53 +0530

    add Hero component to App and update structure

---
+++ src/components/Hero.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <section id='hero'>
+        <div>
+            <h1>MacBook Pro</h1>
+            <img src="/title.png" alt='MacBook Title'/>
+        </div>
+    </section>
+  )
+}
+
+export default Hero
