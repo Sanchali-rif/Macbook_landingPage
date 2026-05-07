3db982a6c3ba54267a095b72655c54841eae3645
Sanchali-rif <sanchalisaha05@gmail.com>
2026-05-07 22:25:37 +0530

    feat: refactor ModelSwitcher component to include MacbookModel16 and enhance presentation controls

--- src/components/ProductViewer.jsx
+++ src/components/ProductViewer.jsx
@@ -5,7 +5,7 @@ import { Canvas } from '@react-three/fiber';
 import { Box, OrbitControls } from '@react-three/drei';
 import MacbookModel14 from './models/Macbook-14';
 import StudioLights from './three/StudioLights';
-import { ModelSwitcher } from './three/ModelSwitcher';
+import ModelSwitcher from './three/ModelSwitcher';
 import { useMediaQuery } from 'react-responsive';
 
 const ProductViewer = () => {
--- src/components/three/ModelSwitcher.jsx
+++ src/components/three/ModelSwitcher.jsx
@@ -1,7 +1,27 @@
+import { PresentationControls } from '@react-three/drei';
 import React from 'react'
+import { useRef } from 'react';
+import MacbookModel14 from '../models/Macbook-14';
+import MacbookModel16 from '../models/Macbook-16';
 
-export const ModelSwitcher = ({scale,isMobile}) => {
+const ModelSwitcher = ( {scale, isMobile}) => {
+    const smallMackbookRef=useRef();
+    const largeMackbookRef=useRef();
+    const showlargeMacbook= scale == 0.08 || scale == 0.05;
   return (
-    <div>ModelSwitcher</div>
+    <>
+        <PresentationControls>
+            <group ref={largeMackbookRef}>
+                <MacbookModel16 scale={isMobile ? 0.05:0.08}/>
+            </group>
+        </PresentationControls>
+        <PresentationControls>
+            <group ref={smallMackbookRef}>
+                <MacbookModel16 scale={isMobile ? 0.03:0.06}/>
+            </group>
+        </PresentationControls>
+    </>
   )
 }
+
+export default ModelSwitcher
