8d14c60b2f4e9221613762babea1a157ef073d3f
Sanchali-rif <sanchalisaha05@gmail.com>
2026-05-07 16:17:23 +0530

    feat: register GSAP ScrollTrigger plugin in App component

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import ProductViewer from './components/ProductViewer'
-
+import { ScrollTrigger } from 'gsap/all'
+import gsap from 'gsap'
+gsap.registerPlugin(ScrollTrigger)
 const App = () => {
   return (
    <main>
