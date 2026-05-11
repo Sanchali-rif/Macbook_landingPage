0253edf1adf2f3a037899b4c47659f53b310400f
Sanchali-rif <sanchalisaha05@gmail.com>
2026-05-11 23:40:03 +0530

    feat: implement Showcase component with video and overlay elements

--- src/components/Showcase.jsx
+++ src/components/Showcase.jsx
@@ -3,7 +3,17 @@ import React from 'react'
 const Showcase = () => {
   return (
     <section id="showcase">
-        Showcase
+        <video src="/videos/game.mp4" loop muted playsInline/>
+        <div className="mask">
+            <img src="/mask-logo.svg"/>
+        </div>
+        <div className='content'>
+            <div className='wrapper'>
+                <div className='lg:max-w-md'>
+                    <h2>Rocket Chip</h2>
+                </div>
+            </div>
+        </div>
     </section>
   )
 }
