2a9177fd681ee95081655f6f20cca6f229706414
Sanchali-rif <sanchalisaha05@gmail.com>
2026-06-09 23:56:31 +0530

    shocase_update

--- src/components/Showcase.jsx
+++ src/components/Showcase.jsx
@@ -11,6 +11,11 @@ const Showcase = () => {
             <div className='wrapper'>
                 <div className='lg:max-w-md'>
                     <h2>Rocket Chip</h2>
+                    
+
+                    <div className="space-y mt-7 pe-10">
+                        <p>Introducing {" "}</p>
+                    </div>
                 </div>
             </div>
         </div>
